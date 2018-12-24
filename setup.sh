b18aa5b9e9b3b606247dc38ac6ac73544eb9d295
David Loyd Jr <dloyd@Davids-new-iMac.local>
2018-12-24 16:36:06 -0500

    updated files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git htop apt-transport-https docker docker-compose
+
+sudo echo "deb https://deb.torproject.org/torproject.org stretch main" >> /etc/apt/sources
+
+gpg2 --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+gpg2 --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+
 apt-get update
-apt-get upgrade
-apt install apt-transport-https htop 
+apt install tor deb.torproject.org-keyring
