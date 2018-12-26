78070334c0d1cdf137f8b3cc0cb89922ac727c10
Dave <dave.e.loyd@icloud.com>
2018-12-26 15:10:44 -0500

    Updated setup

--- setup.sh
+++ setup.sh
@@ -17,14 +17,14 @@ sudo echo "HiddenServicePort 80 127.0.0.1:80" >> /etc/tor/torrc
 
 service tor restart
 cd ~
-git clone https://github.com/reeferdave/stf1.git
+git clone https://github.com/reeferdave/stf-1.git
 
 ONIONHOST=$(cat /var/lib/tor/hidden_service/hostname)
 SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w32 | head -n 1)
 
-echo PUBLIC_IP=$ONIONHOST > ~/stf1/.env
-echo SECRET=$SECRET >> ~/stf1/.env
-echo RETHINKDB_PORT_28015_TCP=tcp://rethinkdb:28015 >> ~/stf1/.env
-echo STATION_NAME=nuc >> ~/stf1/.env
+echo PUBLIC_IP=$ONIONHOST > ~/stf-1/.env
+echo SECRET=$SECRET >> ~/stf-1/.env
+echo RETHINKDB_PORT_28015_TCP=tcp://rethinkdb:28015 >> ~/stf-1/.env
+echo STATION_NAME=nuc >> ~/stf-1/.env
 
-cat ~/stf1/.env
+cat ~/stf-1/.env
